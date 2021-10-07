5dfe091511747e959edb7295a90bc93d5d3a8b44
jacobsonwil <jacobsonwil@urbandaleschools.com>
2021-10-07 10:06:26 -0500

    ch 4 commit

--- 5.2_Roshambo.py
+++ 5.2_Roshambo.py
@@ -10,14 +10,54 @@ Add conditional statements to figure out who wins and keep the records
 When the user quits print a win/loss record
 
 '''
-
-
-
-
-
-
-
-
-
-
-
+import random
+wins = 0
+losses = 0
+ties = 0
+quit = "n"
+comp = 0
+while quit == "n":
+    quit = str(input("Do you want to quit? (press y to quit, n to stay) "))
+    if quit == "y":
+        break
+    elif quit == "n":
+        print()
+    else:
+        print("INPUT THE ACTUAL ANSWER NEXT TIME JACKASS!!")
+    roch = str(input("Rock, Paper or Scisors "))
+    comp = random.randint(1,3)
+    if roch.lower == "rock":
+        roch = 1
+        if comp == 1:
+            print("Tie!")
+            ties = ties + 1
+        elif comp == 2:
+            print("You Lost!")
+            losses = losses + 1
+        else:
+            print("You Won!")
+            wins = wins = 1
+    elif roch.lower == "paper":
+        roch = 2
+        if comp == 1:
+            print("You Won!")
+            wins = wins + 1
+        elif comp == 2:
+            print("Tie!")
+            ties = ties + 1
+        else:
+            print("You Lost!")
+            losses = losses + 1
+    else:
+        roch = 3
+        if comp == 1:
+            print("You Lost!")
+            losses = losses + 1
+        elif comp == 2:
+            print("You Won!")
+            wins = wins + 1
+        else:
+            print("Tie!")
+            ties = ties + 1
+print("You won ", wins, "times, lost ", losses, "times, and you tied the computer ", ties, "times.")
+print("Thanks for playing!")
